ab6982b443484a53d0f3a5385344f065b93e1fed
saadzaoual <saadzaoual@gmail.com>
2025-06-02 10:12:20 +0100

    7th-philosophers--setuping

--- philo/src/time.c
+++ philo/src/time.c
@@ -19,3 +19,22 @@ time_t get_time_in_ms(void)
     gettimeofday(&tv,NULL);
     return((&tv.tv_sec * 1000) + (&tv.tv_usec / 1000));
 }
+
+void	philo_sleep(t_table *table, time_t sleep_time)
+{
+	time_t	wake_up;
+
+	wake_up = get_time_in_ms() + sleep_time;
+	while (get_time_in_ms() < wake_up)
+	{
+		if (has_simulation_stopped(table))
+			break ;
+		usleep(100);
+	}
+}
+
+void	sim_start_delay(time_t start_time)
+{
+	while (get_time_in_ms() < start_time)
+		continue ;
+}
